5437c08cd407b6c6a8db9b63ca0851c86d4d676d
Marina LaForgia <marina.laforgia@gmail.com>
2022-03-31 08:23:53 -0700

    deleted erroneous comment

--- Scripts/20220330_Analysis.R
+++ Scripts/20220330_Analysis.R
@@ -1,6 +1,6 @@
 #### Seed Analysis for CBS Undergraduate Research Conference #### 
 
-# when you read the treatment
+
 Treatment <- read.csv("Data/WE_Treatment.csv")
 
 Treatment$Serpentine <- ifelse(Treatment$Plot %in% c(1,2,7:14,21,41:46,84), 
