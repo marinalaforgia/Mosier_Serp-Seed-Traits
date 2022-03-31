de7fae668f1dae1e4cc6ac796e2b2a847953fe06
Marina LaForgia <marina.laforgia@gmail.com>
2022-03-30 18:57:55 -0700

    added code separating harsh and lush serpentine

--- Scripts/20220330_Analysis.R
+++ Scripts/20220330_Analysis.R
@@ -1,5 +1,10 @@
 #### Seed Analysis for CBS Undergraduate Research Conference #### 
 
-#### hello world ####
+# when you read the treatment
+Treatment <- read.csv("Data/WE_Treatment.csv")
 
-#### Hi Emma! ####
+Treatment$Serpentine <- ifelse(Treatment$Plot %in% c(1,2,7:14,21,41:46,84), 
+                               "HS", 
+                               ifelse(Treatment$Serpentine == "N",
+                                      Treatment$Serpentine,
+                                      "LS"))
