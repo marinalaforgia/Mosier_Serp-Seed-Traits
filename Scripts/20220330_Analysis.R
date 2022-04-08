21d1af08cdcf872489389e37c9c49ae182029957
Marina LaForgia <marina.laforgia@gmail.com>
2022-04-07 18:01:38 -0700

    Commit post meeting with Emma!

--- Scripts/20220330_Analysis.R
+++ Scripts/20220330_Analysis.R
@@ -1,33 +1,62 @@
 #### Seed Analysis for CBS Undergraduate Research Conference #### 
 
-<<<<<<< HEAD
 rm(list=ls())
 
 #library load
+library(plyr)
+library(dplyr)
 library(tidyverse)
 library(ggplot2)
 
 #Data
-Traits<-read.csv("Data/20220329_Seed-Traits_cleaning.csv")
-Cover<-read.csv("Data/Core_All-Years_2021.csv")
-Species<-read.csv("Data/McL_80SitesSpeciesTraits_012615.csv")
-SB<-read.csv("Data/Seedbank-Data_2015 - Seedbank_WateringExp2016_CLEAN.csv")
-Plots<-read.csv("Data/WE_Treatment.csv")
-#Plot 70 came out as 7#
+Traits <- read.csv("Data/20220329_Seed-Traits_cleaning.csv")
+Cover <- read.csv("Data/Core_All-Years_2021.csv")
+Species <- read.csv("Data/McL_80SitesSpeciesTraits_012615.csv")
+SB <- read.csv("Data/Seedbank_WateringExp2016_CLEAN.csv")
+Plots <- read.csv("Data/WE_Treatment.csv") 
 
-#### Joining Tables ####
+#### Prepping Data ####
 
-SB_joined <- left_join(SB,Plots, by = "Plot") %>% select(Plot, Species,Serpentine)
 
-SB_Traits_joined <- left_join(Traits,SB_joined, by = "Species")
+SB.Cov <- c(unique(SB$Species), unique(Cover$Species_Name_J12))
 
-#Just examining data to understand things
-Avena_fatua <- SB_joined %>% filter(Species == "Avena fatua")
+Traits <- merge(Species[,1:2], Traits, by.x = "Species_Name", by.y = "Species")
 
-Treatment <- read.csv("Data/WE_Treatment.csv")
+Traits <- Traits[which(Traits$Species_Name_J12 %in% SB.Cov),]
 
-Treatment$Serpentine <- ifelse(Treatment$Plot %in% c(1,2,7:14,21,41:46,84), 
-                               "HS", 
-                               ifelse(Treatment$Serpentine == "N",
-                                      Treatment$Serpentine,
-                                      "LS"))
+write.csv(Traits, "Data/Trait-subset.csv", row.names = F)
+
+Traits <- read.csv("Data/Trait-subset.csv")
+Traits <- Traits[Traits$Use == "use",]
+
+# Summarize by species
+SB.sum <- ddply(SB, .(Plot, Species), summarize, n.seeds = sum(n_seedlings))
+
+SB_joined <- left_join(SB.sum, Plots, by = "Plot") %>% select(Plot, Species, Serpentine, n.seeds)
+
+SB_Traits_joined <- merge(SB_joined, Traits[,-c(1,3,4,5,6,9,10,11,13,16)],  by.x = "Species", by.y = "Species_Name")
+
+SB_Traits_joined$Serpentine <- ifelse(SB_Traits_joined$Plot %in% c(1,2,7:14,21,41:46,84), 
+                               "HS", # Harsh serpentine
+                               ifelse(SB_Traits_joined$Serpentine == "N",
+                                      SB_Traits_joined$Serpentine,
+                                      "LS")) # Lush serpentine
+
+
+
+#### To do ###
+# 1. Draw graphs!
+# 2. Try to recreate graphs with ggplot2 and ASK marina when you get confused :D
+# 3. CWM
+ ### Sum all individuals in a plot total seeds
+ ### divide number of seeds for a specific species by total (RA)
+ ### multiple trait by that RA
+ ### take the mean of the plot, functional group (native, invasive, forb, grass, NF, IG)
+# Community level ordinations by soil type 
+pca <- prcomp(df, scale = T) # only includes quantitative data
+summary(pca)
+biplot(pca)
+
+#split graph by serpentine type and color by forbs/grass/invasive/natives
+
+### 
