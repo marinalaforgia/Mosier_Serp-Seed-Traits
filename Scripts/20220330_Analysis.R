04deb6eb1b432ef117760b617619c6003b358b72
Marina LaForgia <marina.laforgia@gmail.com>
2022-04-14 16:42:25 -0700

    transformations

--- Scripts/20220330_Analysis.R
+++ Scripts/20220330_Analysis.R
@@ -8,7 +8,7 @@ library(dplyr)
 library(tidyverse)
 library(ggplot2)
 library(devtools)
-install_github("vqv/ggbiplot")
+install_github("vqv/ggbiplot", force = T)
 
 #Data
 Traits <- read.csv("Data/20220329_Seed-Traits_cleaning.csv")
@@ -42,7 +42,7 @@ SB.sum <- ddply(SB, .(Plot, Species), summarize, n.seeds = sum(n_seedlings))
 
 SB_joined <- left_join(SB.sum, Plots, by = "Plot") %>% select(Plot, Species, Serpentine, n.seeds)
 
-SB_Traits_joined <- merge(SB_joined, Traits[,-c(1,3,4,5,6,9,10,11,13,16)],  by.x = "Species", by.y = "Species_Name")
+SB_Traits_joined <- merge(SB_joined, Traits[,-c(1,4,5,6,9,10,11,13,16)],  by.x = "Species", by.y = "Species_Name_J12")
 
 #Remove NA data
 SB_Traits_joined <- SB_Traits_joined[complete.cases(SB_Traits_joined),]
@@ -63,7 +63,7 @@ Plot.cwm <- SB_Traits_joined            %>% group_by(Plot)                     %
 Plot.cwm <- merge(Plot.cwm, Plots[,c(1,3)], by = "Plot")
 
 ####_Native/Invasive####
-nat.inv.cwm <- SB_Traits_joined %>% group_by(nat.inv) %>% summarize(
+nat.inv.cwm <- SB_Traits_joined %>% group_by(nat.inv, Plot) %>% summarize(
   shape.final.cwm = weighted.mean(shape.final, n.seeds),
   set.time.mpsec.cwm = weighted.mean(set.time.mpsec, n.seeds),
   mass.morph.mg.cwm = weighted.mean(mass.morph.mg, n.seeds),
@@ -71,6 +71,8 @@ nat.inv.cwm <- SB_Traits_joined %>% group_by(nat.inv) %>% summarize(
   height.cm.cwm = weighted.mean(height.cm, n.seeds),
   ldd.cwm = weighted.mean(ldd, n.seeds)
 )
+
+nat.inv.cwm <- merge(nat.inv.cwm, Plots[,c(1,3)], by = "Plot")
 
 ####_Forb/Grass####
 group.cwm <- SB_Traits_joined %>% group_by(group) %>% summarize(
@@ -89,16 +91,34 @@ library(factoextra)
 library(ggbiplot)
 
 #PCA
-Plot.pca <- prcomp(Plot.cwm[,c(1:7)], scale = TRUE)
+hist(log(Plot.cwm$shape.final.cwm))
+hist(sqrt(Plot.cwm$set.time.mpsec.cwm))
+hist(log(Plot.cwm$mass.morph.mg.cwm))
+hist(log(Plot.cwm$wing.loading.cwm))
+hist(Plot.cwm$height.cm.cwm)
+hist(sqrt(Plot.cwm$ldd.cwm))
+
+Plot.cwm$shape.final.cwm.log <- log(Plot.cwm$shape.final.cwm)
+Plot.cwm$set.time.mpsec.cwm.sqrt <- sqrt(Plot.cwm$set.time.mpsec.cwm)
+Plot.cwm$mass.morph.mg.cwm.log <- log(Plot.cwm$mass.morph.mg.cwm)
+Plot.cwm$wing.loading.cwm.log <- log(Plot.cwm$wing.loading.cwm)
+Plot.cwm$ldd.cwm.sqrt <- sqrt(Plot.cwm$ldd.cwm)
+
+Plot.pca <- prcomp(Plot.cwm[,c(6,9:13)], scale = TRUE)
 
 summary(Plot.pca)
 biplot(Plot.pca)
 
 ####_ggbiplot####
-Plot.serp<- Plot.cwm$Serpentine
+Plot.serp <- Plot.cwm$Serpentine
 
 ggbiplot(Plot.pca, obs.scale = 1, var.scale = 1,
          groups = Plot.serp)
+
+Plot.cwm <- cbind(Plot.cwm, Plot.pca$x[,1:2])
+
+autoplot(Plot.pca, data = Plot.cwm, loadings = T, loadings.label = T, label = F, loadings.label.vjust = -1.5, loadings.label.hjust = .6, col = 'appendages', shape = 'group') +
+  theme_classic()  
 
 ####_factoextra####
 
@@ -113,8 +133,12 @@ fviz_pca_var(Plot.pca,
 ####Barplots####
 
 #Basic Barplot
-Barplot <- ggplot(data = Plot.cwm, mapping = aes(x = Serpentine, y = shape.final.cwm)) + geom_bar(stat="identity")
+library(Rmisc)
+
+Barplot <- ggplot(data = Plot.cwm, mapping = aes(x = Serpentine, y = shape.final.cwm)) + geom_boxplot()
 Barplot
+
+ggplot(data = nat.inv.cwm, mapping = aes(x = Serpentine, y = shape.final.cwm, col = nat.inv)) + geom_boxplot()
 
 #Stacked Barplot
 
